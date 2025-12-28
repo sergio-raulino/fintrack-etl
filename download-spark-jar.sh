fe58f4cb936aa0761dab836f887247e36ee17805
22666 <sergio.raulino@tjce.jus.br>
2025-12-28 17:17:28 -0300

    create shell script for spark's jars

---
+++ download-spark-jar.sh
@@ -0,0 +1,21 @@
+mkdir -p ~/spark/jars
+cd ~/spark/jars
+
+# AWS SDK
+wget https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.12.734/aws-java-sdk-bundle-1.12.734.jar
+
+# Hadoop 3.3.6
+wget https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-auth/3.3.6/hadoop-auth-3.3.6.jar
+wget https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-common/3.3.6/hadoop-common-3.3.6.jar
+wget https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-client-api/3.3.6/hadoop-client-api-3.3.6.jar
+wget https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-client-runtime/3.3.6/hadoop-client-runtime-3.3.6.jar
+
+# Spark Hive 3.5.4 (Scala 2.12)
+wget https://repo1.maven.org/maven2/org/apache/spark/spark-hive_2.12/3.5.4/spark-hive_2.12-3.5.4.jar
+
+# Iceberg (Spark Runtime 3.5 — Scala 2.12)
+wget https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-spark-runtime-3.5_2.12/1.7.2/iceberg-spark-runtime-3.5_2.12-1.7.2.jar
+
+# StAX & Woodstox
+wget https://repo1.maven.org/maven2/com/fasterxml/woodstox/stax2-api/4.2.1/stax2-api-4.2.1.jar
+wget https://repo1.maven.org/maven2/com/fasterxml/woodstox/woodstox-core/6.5.1/woodstox-core-6.5.1.jar
